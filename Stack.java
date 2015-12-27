a5f8c294fe4c945ad43ab7b964008351bf499f0a
AnnaBoro <anna_bo@list.ru>
2015-12-27 21:47:58 +0200

    mystack

---
+++ Stack.java
@@ -0,0 +1,36 @@
+package lesson7.stack;
+
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class Stack {
+
+
+    private LinkedList stackL;
+
+    public Stack() {
+
+        stackL = new LinkedList();
+    }
+
+    public void push(Object o) {
+
+        stackL.addLast(o);
+    }
+
+    public Object pop() {
+
+        if (stackL.isEmpty()) {
+            return new NoSuchElementException();
+        }
+        Object obj  = stackL.getLast();
+        stackL.removeLast();
+        return obj;
+    }
+
+    public Object peek() {
+
+        return stackL.getLast();
+    }
+}
